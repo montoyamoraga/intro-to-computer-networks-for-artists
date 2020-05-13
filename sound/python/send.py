771c35348445edd1a1a297f77b92f1539bfbdaa1
montoyamoraga <montoyamoraga@gmail.com>
2020-05-13 13:39:55 -0400

    add python example

--- sound/python/send.py
+++ sound/python/send.py
@@ -1,7 +1,14 @@
+# import Python modules
 import time
 import requests
 
 # play one chromatic octave
 for i in range(60, 71):
-    requests.get("http://216.180.89.221:12345/?type=noteOn&channel=1&noteNumber=%d&value=90" % i)
-    time.sleep(0.2)
+  note = str(i)
+  url = "http://localhost:9000/?type=noteOn&channel=2&number=" + note + "&value=90"
+  requests.get(url)
+  # time between each note
+  time.sleep(0.2)
+
+# value 0 to mute
+requests.get("http://localhost:9000/?type=noteOn&channel=2&number=0&value=0")
